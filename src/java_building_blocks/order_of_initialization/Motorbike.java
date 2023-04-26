b21d58be0e10cc5283d996732497ec6e8bf266e6
grfira <grfira@users.noreply.github.com>
2023-04-26 10:54:36 +0200

    refactored code of Motorbike.java

--- src/java_building_blocks/order_of_initialization/Motorbike.java
+++ src/java_building_blocks/order_of_initialization/Motorbike.java
@@ -1,18 +1,19 @@
 package src.java_building_blocks.order_of_initialization;
 class Motorbike extends Bike{
-    {System.out.print("1 ");}
-    Motorbike(int num) {super(num);System.out.print("2 ");}
+    {print(1);}
+    Motorbike(int num) {super(num);print(2);}
     public static void main(String[] args) {
-        System.out.print("3 ");
+        print(3);
         new Motorbike(0);
-        System.out.print("4 ");
-        {System.out.print("5 ");}
+        print(4);
+        {print(5);}
     }
-    static {System.out.print("6 ");}
+    static {print(6);}
 }
 class Bike{
-    {System.out.print("7 ");}
-    Bike(){super();System.out.print("8 ");}
-    Bike(int num) {this();System.out.print("9 ");}
-    static {System.out.print("10 ");    }
+    static void print(int number){System.out.print(number+" ");}
+    {print(7);}
+    Bike(){super();print(8);}
+    Bike(int num) {this();print(9);}
+    static {print(10);    }
 }
