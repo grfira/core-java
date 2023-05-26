630ddbae7d925a2cf52ab1222c5ed50570bd0f41
grfira <grfira@users.noreply.github.com>
2023-05-27 00:36:35 +0200

    updated OperatorPrecedence.java - added multiplication and modulus operators

--- src/operators_and_statements/OperatorPrecedence.java
+++ src/operators_and_statements/OperatorPrecedence.java
@@ -3,13 +3,13 @@ package src.operators_and_statements;
 class OperatorPrecedence {
     public static void main(String[] args) {
 
+        System.out.println(7 / 5 * 3);
         int x = 5;
         System.out.println(x + x * x + x);
         System.out.println(x += x *= x += x);
         byte b = 0b100;
         System.out.println(~b | b & b);
-        System.out.println(true||false&&false);
-
+        System.out.println(true || false && false);
 
 
     }
